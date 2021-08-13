d9218e9a43b4e4b0f8d227bfe3cb3ab8fd5a87c9
wahabmangat <abdulwahabmangat@gmail.com>
2021-08-13 11:39:57 +0500

    Revert "Add devise gem and update extension of stylesheets."
    This reverts commit 29c44be038db6855e7f91d6b08d46c64781fad3c.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   root "welcome#index"
   get '/welcome', to: "welcome#index"
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
-  }
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
