2cf098cca5cd51a9fe46ea6f7b3ae9bc749f277e
wahabmangat <abdulwahabmangat@gmail.com>
2021-08-16 22:21:11 +0500

    Change the normal form request to AJAX request

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -4,7 +4,9 @@ class StocksController < ApplicationController
             flash[:notice] = "Stock information found"
             @stock = Stock.new_lookup(params[:stock])
             if @stock
-                render "users/my_portfolio"
+                respond_to do |format|
+                    format.js { render  partial: 'users/result'}
+                end
             else
                 flash[:alert] = "Please enter a valid symbol to search"
                 redirect_to my_portfolio_path
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,6 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "@fortawesome/fontawesome-free/css/all"
-
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
