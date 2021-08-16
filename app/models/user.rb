4815b27e6b20312c7cf74f7d6b07f87d5d08baec
wahabmangat <abdulwahabmangat@gmail.com>
2021-08-16 11:35:54 +0500

    Add first/lastname to devise users and partial confirmable implementation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
+  #attr_accessible :first_name, :last_name
+  # validates :first_name, presence: true
+  # validates :last_name, presence: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+           
+  validates :first_name, :last_name, presence: true
 end
