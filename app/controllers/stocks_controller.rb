055eaedce17825972e7008ea9c3df934f4de0280
wahabmangat <abdulwahabmangat@gmail.com>
2021-08-17 11:36:51 +0500

    Add validations to AJAX form and update environment.js.

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,19 +1,23 @@
 class StocksController < ApplicationController
-    def search
-        if params[:stock].present?
-            flash[:notice] = "Stock information found"
-            @stock = Stock.new_lookup(params[:stock])
-            if @stock
-                respond_to do |format|
-                    format.js { render  partial: 'users/result'}
-                end
-            else
-                flash[:alert] = "Please enter a valid symbol to search"
-                redirect_to my_portfolio_path
-            end
-        else 
-            flash[:alert] = "Please enter the symbol to search"
-            redirect_to my_portfolio_path
-        end
-    end
+	def search
+		if params[:stock].present?
+			@stock = Stock.new_lookup(params[:stock])
+			if @stock.present?
+				respond_to do |format|
+					flash.now[:notice] = "Stock information found"
+					format.js { render  partial: 'users/result'}
+				end
+			else
+				respond_to do |format|
+				    flash.now[:alert] = "Please enter a valid symbol to search"
+					format.js { render  partial: 'users/result'}
+				end
+			end
+		else 
+			respond_to do |format|
+				flash.now[:alert] = "Please enter a symbol to search"
+				format.js { render  partial: 'users/result'}
+			end
+		end
+	end
 end
