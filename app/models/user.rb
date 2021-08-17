dbb5f8eae78bc9b241674707260ff55efc39ac09
wahabmangat <abdulwahabmangat@gmail.com>
2021-08-17 13:56:34 +0500

    Add User_stock resource and associations in stock,user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :user_stocks
+  has_many :stocks, through: :user_stocks
   #attr_accessible :first_name, :last_name
   # validates :first_name, presence: true
   # validates :last_name, presence: true
