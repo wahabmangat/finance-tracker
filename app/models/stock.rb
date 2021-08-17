e261ee3e213e0bf9071fdcc65691cef6d5e6e04f
wahabmangat <abdulwahabmangat@gmail.com>
2021-08-17 17:39:59 +0500

    Add track stock functionality to user's portfolio.

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,16 +1,19 @@
 class Stock < ApplicationRecord
-    has_many :user_stocks
-    has_many :users, through: :user_stocks
-    validates :name, :ticker, presence: true
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+  validates :name, :ticker, presence: true
     def self.new_lookup(ticker_symbol)
-        client = IEX::Api::Client.new(
-            publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
-            endpoint: 'https://sandbox.iexapis.com/v1'
-          )
-        begin
-            new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
-        rescue => exception
-            return nil
-        end
+      client = IEX::Api::Client.new(
+        publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
+        endpoint: 'https://sandbox.iexapis.com/v1'
+      )
+      begin
+        new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+			rescue => exception
+        return nil
+      end
     end
+		def self.check_db(ticker_symbol)
+			find_by_ticker(ticker_symbol)
+		end   
 end
