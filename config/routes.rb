6b8d0cd57e1bd1bbdee486c4fb5f8a5428d3946e
wahabmangat <abdulwahabmangat@gmail.com>
2021-08-19 14:07:51 +0500

    Add edit user info.

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ Rails.application.routes.draw do
   root "welcome#index"
   get '/welcome', to: "welcome#index"
   devise_for :users, controllers: {
-    sessions: 'users/sessions'
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
   }
   get "/my_portfolio", to: "users#my_portfolio"
   get '/search_stock', to: "stocks#search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
