77c5cdb954097f1cb52ff67ba55115fb9cc3629c
wahabmangat <abdulwahabmangat@gmail.com>
2021-08-20 14:57:54 +0500

    Position the search friend button.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,15 +29,15 @@ class UsersController < ApplicationController
       @q = User.ransack(params[:q])
       @friend =@q.result(distinct: true)
       @friend = current_user.except_current_user(@friend)
-        unless @friend.empty?
-          respond_to do |format|
+      unless @friend.empty?
+        respond_to do |format|
 					flash.now[:notice] = "Friend information found"
 					format.js { render  partial: 'users/friend_result'}
 				end
 			else
 				respond_to do |format|
-				    flash.now[:alert] = "Couldn't find user."
-					  format.js { render  partial: 'users/friend_result'}
+				  flash.now[:alert] = "Couldn't find user."
+					format.js { render  partial: 'users/friend_result'}
 				end
 			end
 		else 
