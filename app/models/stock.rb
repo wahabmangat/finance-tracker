c52b183868921a33536ab917ac9d5788094363ec
wahabmangat <abdulwahabmangat@gmail.com>
2021-08-21 17:09:53 +0500

    Code refactor using scopes instead of class methods.

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,6 +2,10 @@ class Stock < ApplicationRecord
   has_many :user_stocks
   has_many :users, through: :user_stocks
   validates :name, :ticker, presence: true
+  
+  scope :sort_ascend_by_price, -> { order(:last_price) }
+  scope :sort_descend_by_price, -> { order(last_price: :desc) }
+
 
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(
@@ -15,7 +19,8 @@ class Stock < ApplicationRecord
       return nil
     end
   end
-
+   
+  
   def self.check_db(ticker_symbol)
     find_by_ticker(ticker_symbol)
   end
@@ -32,14 +37,12 @@ class Stock < ApplicationRecord
     end 
   end 
   
-  private
+  # def self.sort_ascend_by_price
+  #   order(:last_price)
+  # end
+
+  # def self.sort_descend_by_price
+  #   order(last_price: :desc)
+  # end 
   
-  def self.sort_ascend_by_price
-    order(:last_price)
-  end
-
-  def self.sort_descend_by_price
-    order(last_price: :desc)
-  end 
-
 end
